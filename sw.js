f28a1ff1685d09d1d3ef165d30034fbf817e3bfe
dosherm <dans.home.23@gmail.com>
2025-11-24 13:49:56 -0600

    Add files via upload

--- sw.js
+++ sw.js
@@ -9,14 +9,25 @@ const ASSETS = [
   "./icon-192.png",
   "./icon-512.png"
 ];
+
 self.addEventListener("install", (event) => {
   event.waitUntil(caches.open(CACHE).then((c) => c.addAll(ASSETS)));
+  self.skipWaiting();
 });
+
 self.addEventListener("activate", (event) => {
-  event.waitUntil(caches.keys().then(keys =>
-    Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
-  ));
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    ).then(() => self.clients.claim())
+  );
+  event.waitUntil(
+    self.clients.matchAll({ type: "window" }).then((clients) => {
+      clients.forEach((client) => client.navigate(client.url));
+    })
+  );
 });
+
 self.addEventListener("fetch", (event) => {
   event.respondWith(caches.match(event.request).then((cached) => cached || fetch(event.request)));
 });
